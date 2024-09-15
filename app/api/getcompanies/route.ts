209c343e736aa93d55e40ac293476d2246db2eb6
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-15 14:24:21 +0530

    sending company names in formatted and sorted order

--- app/api/getcompanies/route.ts
+++ app/api/getcompanies/route.ts
@@ -3,11 +3,17 @@ import { promises as fs } from "fs";
 
 import { NextResponse } from "next/server";
 
+const capitalizeFirstLetter = (name: string): string => {
+  return name.charAt(0).toUpperCase() + name.slice(1);
+};
+
 export async function GET() {
   try {
     const questionsDirectory = path.join(process.cwd(), "config", "questions");
     const files = await fs.readdir(questionsDirectory);
-    const filenames = files.map((filename) => path.parse(filename).name);
+    const filenames = files
+      .map((filename) => capitalizeFirstLetter(path.parse(filename).name))
+      .sort();
 
     return NextResponse.json({ companies: filenames }, { status: 200 });
   } catch (error) {
