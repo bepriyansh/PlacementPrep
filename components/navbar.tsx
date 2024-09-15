21e7fd84d48dfc60328e660a546d6203f6a5531f
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-15 11:17:17 +0530

    initial config for site

---
+++ components/navbar.tsx
@@ -0,0 +1,67 @@
+import {
+  Navbar as NextUINavbar,
+  NavbarContent,
+  NavbarBrand,
+  NavbarItem,
+} from "@nextui-org/navbar";
+import { Kbd } from "@nextui-org/kbd";
+import { Link } from "@nextui-org/link";
+import { Input } from "@nextui-org/input";
+import NextLink from "next/link";
+import { Image } from "@nextui-org/image";
+
+import { siteConfig } from "@/config/site";
+import { GithubIcon, SearchIcon } from "@/components/icons";
+
+export const Navbar = () => {
+  const searchInput = (
+    <Input
+      aria-label="Search"
+      classNames={{
+        inputWrapper: "bg-default-100",
+        input: "text-sm",
+      }}
+      labelPlacement="outside"
+      placeholder="Search..."
+      startContent={
+        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0 mr-1" />
+      }
+      type="search"
+    />
+  );
+
+  return (
+    <NextUINavbar maxWidth="xl" position="sticky">
+      <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
+        <NavbarBrand as="li" className="gap-3 max-w-fit">
+          <NextLink className="flex justify-start items-center gap-1" href="/">
+            <Image
+              alt="DSA"
+              className="h-8 my-2 rounded-none"
+              src={siteConfig.logo}
+            />
+          </NextLink>
+        </NavbarBrand>
+      </NavbarContent>
+      <NavbarContent className="flex justify-center items-center">
+        <NavbarItem className="w-full">{searchInput}</NavbarItem>
+      </NavbarContent>
+
+      <NavbarContent
+        className="hidden sm:flex basis-1/5 sm:basis-full"
+        justify="end"
+      >
+        <NavbarItem className="flex gap-2">
+          <Link
+            isExternal
+            aria-label="Github"
+            href={siteConfig.links.github}
+            title="Feel free to contribute"
+          >
+            <GithubIcon className="text-default-500" />
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+    </NextUINavbar>
+  );
+};
---
+++ config/site.ts
@@ -0,0 +1,16 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Next.js + NextUI",
+  description: "Make beautiful websites regardless of your design experience.",
+  logo: "/images/dsa_logo.jpg",
+  navItems: [
+    {
+      label: "Home",
+      href: "/",
+    },
+  ],
+  links: {
+    github: "https://github.com/priyaaanshh/PlacementPrep",
+  },
+};
