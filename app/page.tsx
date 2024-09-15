41a5ab08d39e9d5ba974e10ff44af9fa0c570ab5
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-09-15 19:26:09 +0530

    added searchbar to search companies

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,51 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { Accordion, AccordionItem } from "@nextui-org/accordion";
+import { Input } from "@nextui-org/input";
 
 import QuestionList from "@/components/questionList";
 import { CompaniesData } from "@/config/companies";
+import { SearchIcon } from "@/components/icons";
 
 const Page = () => {
+  // State to store the search query
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Function to handle search input change
+  const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchQuery(event.target.value.toLowerCase());
+  };
+
+  // Filter companies based on search query
+  const filteredCompanies = CompaniesData.filter((company) =>
+    company.name.toLowerCase().includes(searchQuery),
+  );
+
+  const searchInput = (
+    <Input
+      aria-label="Search"
+      classNames={{
+        inputWrapper: "bg-default-100",
+        input: "text-sm",
+      }}
+      labelPlacement="outside"
+      placeholder="Search..."
+      startContent={
+        <SearchIcon className="text-base text-default-400 pointer-events-none flex-shrink-0 mr-1" />
+      }
+      type="search"
+      value={searchQuery}
+      onChange={handleSearchChange}
+    />
+  );
+
   return (
     <div>
-      <Accordion>
-        {CompaniesData.map((company, i) => (
+      <div className="flex justify-end w-full">
+        <div className="w-full max-w-[400px] mb-5">{searchInput}</div>
+      </div>
+      <Accordion selectionMode="multiple" variant="bordered">
+        {filteredCompanies.map((company, i) => (
           <AccordionItem
             key={i}
             aria-label={company.name}
