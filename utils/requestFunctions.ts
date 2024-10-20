94b110eaacc4c102e81fbf06ddc2f50a778d882e
priyaaanshh <priyansh.sahu.cer21@itbhu.ac.in>
2024-10-20 10:43:49 +0530

    Using server functions instead of making api request

--- utils/requestFunctions.ts
+++ utils/requestFunctions.ts
@@ -1,26 +1,81 @@
+"use server";
+import path from "path";
+import fs from "fs";
+
+import csvParser from "csv-parser";
+
 import { CompanyQuestion } from "@/types/interfaces";
 
-const getQuestions = async (company: string): Promise<CompanyQuestion[]> => {
+// // Discontinued this implementation
+// const getQuestions = async (company: string): Promise<CompanyQuestion[]> => {
+//   try {
+//     const res = await fetch("/api/getquestions", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify(company),
+//     });
+
+//     if (!res.ok) {
+//       return [];
+//     }
+//     const data = await res.json();
+
+//     return data.questions;
+//   } catch (error) {
+//     return [];
+//   }
+
+//   return [];
+// };
+
+const validateNumberString = (num: string): Boolean => {
+  for (let i = 0; i < num.length; i++) {
+    if (num[i] < "0" || num[i] > "9") return false;
+  }
+
+  return true;
+};
+
+// using server functions directly for fetching the questions
+// instead of implementing apis
+const getQuestions = async (name: String) => {
+  const companyName = name + ".csv";
+  const difficulty = ["Medium", "Easy", "Hard", ""];
+
   try {
-    const res = await fetch("/api/getquestions", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(company),
+    const questionsDirectory = path.join(process.cwd(), "config", "questions");
+
+    const filePath = path.join(questionsDirectory, companyName);
+
+    const results: CompanyQuestion[] = [];
+
+    await new Promise<CompanyQuestion[]>((resolve, reject) => {
+      let index = 1;
+
+      fs.createReadStream(filePath)
+        .pipe(csvParser())
+        .on("data", (data) => {
+          data.difficulty = data.difficulty.trim();
+          if (!difficulty.includes(data.difficulty)) data.difficulty = "";
+          if (!validateNumberString(data.num_occur)) {
+            data.num_occur = "1";
+          }
+          results.push({ ...data, index: index++ });
+        })
+        .on("end", () => {
+          resolve(results);
+        })
+        .on("error", (error) => {
+          reject(error);
+        });
     });
 
-    if (!res.ok) {
-      return [];
-    }
-    const data = await res.json();
-
-    return data.questions;
+    return results;
   } catch (error) {
     return [];
   }
-
-  return [];
 };
 
 export { getQuestions };
